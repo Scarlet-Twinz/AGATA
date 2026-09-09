469e08dc0a63ced3157002ea731f7c6caf389b11
Anthony Emmanuella Mmasinachi <anthonyemmanuella297@gmail.com>
2026-09-09 04:43:26 -0700

    Apply final public logo fix

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./styles.css";
 import "./public-polish.css";
+import "./final-public-fixes.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
